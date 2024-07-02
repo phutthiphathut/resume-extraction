0448796b6eb556344c6ff073c71946796bbf4e1a
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-07-03 00:32:48 +0700

    add document files and init server file

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from pyresparser import ResumeParser
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/resume/extract")
+def read_root():
+    return read_doc()
+
+def read_doc():
+    data = ResumeParser("C:/Users/ASUS/OneDrive/Desktop/senior-project/resume-extraction/src/resume-sample.pdf").get_extracted_data()
+    return data
+
