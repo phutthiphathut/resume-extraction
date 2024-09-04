9cb4aa93e4eff2c9fbd2498247dea1314d479126
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-09-04 23:59:18 +0700

    update file main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-from routers import resume_router
+from routers import job_seeker_router, profile_router, recruiter_router
 
 app = FastAPI()
 
-app.include_router(resume_router)
-
-@app.get("/")
-async def root():
-    return {"message": "Resume Extraction Service is Healthy!"}
+app.include_router(job_seeker_router.router)
+app.include_router(profile_router.router)
+app.include_router(recruiter_router.router)
