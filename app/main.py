d25aa229af33d6ba21f0503171d6405ca9b92357
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-09-10 00:25:19 +0700

    add middleware when initial app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from configs.app_config import appConfig
 from routers import job_seeker_router, profile_router, recruiter_router
 
 app = FastAPI()
+
+origins = [appConfig.CLIENT_ORIGIN_URL]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(job_seeker_router.router)
 app.include_router(profile_router.router)
