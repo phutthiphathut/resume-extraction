f62388bc22d54423f004896366aa595c2b2b56a7
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-09-12 00:44:27 +0700

    add validator in register job seeker request

--- app/models/requests/job_seeker_request.py
+++ app/models/requests/job_seeker_request.py
@@ -1,5 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, field_validator
+
 
 class RegisterJobSeekerRequest(BaseModel):
-    email: str
+    email: EmailStr
     password: str
+
+    @field_validator('password')
+    def validate_password(cls, value):
+        if len(value) < 8:
+            raise ValueError('Password must be at least 8 characters long')
+        if not any(char.isalpha() for char in value):
+            raise ValueError('Password must contain at least one letter')
+        if not any(char.isdigit() for char in value):
+            raise ValueError('Password must contain at least one number')
+
+        return value
+
+    class Config:
+        str_strip_whitespace = True
