f55ce829bbd0ec4a89d73f47fb8ffc15a7f46a60
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-09-12 00:43:21 +0700

    update mongodb connection

--- app/databases/mongo_db.py
+++ app/databases/mongo_db.py
@@ -1,9 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+
 from configs.app_config import appConfig
 
 mongo_client = AsyncIOMotorClient(appConfig.MONGO_DB_URL)
 print("Connected to MongoDB successfully!")
 
 db = mongo_client[appConfig.MONGO_DB_NAME]
-
-JobSeekerCollection = db["job_seekers"]
