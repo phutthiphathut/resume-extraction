019273f80890d2d0883912982e3f24bd9cf68c5b
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-09-16 00:12:33 +0700

    add new get expiration time method in datetime util

--- app/utils/datetime_util.py
+++ app/utils/datetime_util.py
@@ -1,7 +1,11 @@
 import pytz
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class DatetimeUtil:
     @staticmethod
     def get_local_datetime() -> datetime:
         return datetime.now(pytz.timezone('Asia/Bangkok'))
+    
+    @staticmethod
+    def get_expiration_time(minutes: int) -> datetime:
+        return datetime.now() + timedelta(minutes=minutes)
