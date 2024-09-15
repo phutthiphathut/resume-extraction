db2f027f84de201edc088ab2a94a10bdbc4b41d0
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-09-16 00:12:55 +0700

    add jwt in app config

--- app/configs/app_config.py
+++ app/configs/app_config.py
@@ -6,6 +6,9 @@ class AppConfig(BaseSettings):
 
     CLIENT_ORIGIN_URL: str
 
+    JWT_SECRET_KEY: str
+    JWT_EXPIRE_MINUTES: int
+
     model_config = SettingsConfigDict(env_file='.env')
 
 appConfig = AppConfig()
