e71d7cdee0cb06d4a2eb07319fde26044589563d
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-09-15 17:13:17 +0700

    fix id issue in mongo base model

--- app/models/collections/mongo_base_model.py
+++ app/models/collections/mongo_base_model.py
@@ -1,30 +1,24 @@
 from datetime import datetime
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from bson import ObjectId
 from typing import Optional
 
 from utils.datetime_util import get_local_datetime
 
 
-class PyObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError('Invalid ObjectId')
-        return ObjectId(v)
-
-
 class MongoBaseModel(BaseModel):
-    id: Optional[PyObjectId] = Field(default_factory=PyObjectId, alias="_id")
+    id: Optional[ObjectId] = Field(default_factory=ObjectId, alias="_id")
     created_at: datetime = Field(default_factory=get_local_datetime)
     updated_at: datetime = Field(default_factory=get_local_datetime)
 
     def update_timestamp(self):
         self.updated_at = get_local_datetime()
+
+    @field_validator("id")
+    def validate_object_id(cls, value: str) -> ObjectId:
+        if not ObjectId.is_valid(value):
+            raise ValueError("Invalid ObjectId")
+        return ObjectId(value)
 
     class Config:
         json_encoders = {ObjectId: str}
