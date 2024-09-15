f245ddf74d8971dc0d3dc74f1752ce9ff85e5bd8
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-09-15 17:11:00 +0700

    remove comment

--- app/utils/datetime_util.py
+++ app/utils/datetime_util.py
@@ -1,8 +1,6 @@
 import pytz
 from datetime import datetime
 
-# Function to get local datetime in Thailand timezone
-
 
 def get_local_datetime() -> datetime:
     return datetime.now(pytz.timezone('Asia/Bangkok'))
