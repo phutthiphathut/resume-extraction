71201c2231d770c3bdc5f3c5df10d4df5248b1b2
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-09-15 17:11:16 +0700

    add password utils

---
+++ app/utils/password_util.py
@@ -0,0 +1,9 @@
+import bcrypt
+
+def hash(password: str) -> str:
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    
+    return hashed.decode('utf-8')
+
+def verify(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
