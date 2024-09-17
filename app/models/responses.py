c97f607799165588923cf6963d8a8838c93cad32
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-09-18 00:34:02 +0700

    refactor upload job seeker response model

--- app/models/responses.py
+++ app/models/responses.py
@@ -1,33 +1,31 @@
 from typing import Any, Generic, List, Optional, TypeVar
 from pydantic import BaseModel
 
+from models.profile import Profile
+
 T = TypeVar('T')
+
 
 class BaseResponse(BaseModel):
     status_code: int
     status_message: Any
 
+
 class SuccessResponse(BaseResponse, Generic[T]):
     data: Optional[T] = None
+
 
 class FailResponse(BaseResponse):
     pass
 
+
 class LoginJobSeekerResponseData(BaseModel):
     access_token: str
 
+
 class UploadJobSeekerResumeResponseData(BaseModel):
-    name: str
-    email: str
-    mobile_number: str
-    skills: List[str]
-    college_name: Optional[str] = None
-    degree: List[str]
-    designation: List[str]
-    experience: List[str]
-    company_names: List[str]
-    no_of_pages: int
-    total_experience: float
+    profile: Profile
+
 
 class LoginRecruiterResponseData(BaseModel):
     access_token: str
